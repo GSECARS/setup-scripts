dabba3b542048719b32a69797f87841f14671bed
Christofanis Skordas <55315750+skordaschristofanis@users.noreply.github.com>
2023-03-03 15:04:05 -0600

    Fixed install.sh file

--- genx3/scripts/install.sh
+++ genx3/scripts/install.sh
@@ -1,25 +1,82 @@
 #!/bin/bash
+# clear terminal screen before starting
+clear
+
+echo -ne "
+----------------------------------------
+Preparing anaconda environment for GenX3
+----------------------------------------
+
+"
 
 # Load the genx3.conf file
 source genx3.conf
 
-# Target file names
-genx3_file_name="start_genx3.sh"
-sbatch_file_name="sbatch_genx3.example.sh"
+# Start time
+SECONDS=0
+echo -ne "
+Anaconda configuration starting
 
-# Copy and rename the template files
-cp "templates/start_genx3.template" "$genx3_file_name"
-cp "templates/sbatch_genx3.example.template" "$sbatch_file_name"
+"
 
-# Change the contents of the GenX3 start file with the correct values
-sed -i "s|module load|module load python/$anaconda_module|g" "$genx3_file_name"
-sed -i "s|source|source '/software/python-$anaconda_module-el7-x86_64/etc/profile.d/conda.sh'|g" "$genx3_file_name"
-sed -i "s|conda activate|conda activate $environment_name|g" "$genx3_file_name"
+# Anaconda environment
+echo "Setting up anaconda environment?"
+# conda.sh
+source "/software/python-$anaconda_module-el7-x86_64/etc/profile.d/conda.sh"
+# envs directory path
+anaconda_envs_path="$HOME/.conda/envs"
+# create full path for the environment
+full_environment_path="$anaconda_envs_path/$environment_name"
+# terminate if the environment exists
+if [ -d "$full_environment_path" ];
+then
+    echo -ne "The environment '$environment_name' already exists. Aborting..."
+    sleep 1
+    exit 0
+fi
 
-# Change the contents of the GenX3 SBATCH file with the correct values
-sed -i "s|module load|module load python/$anaconda_module|g" "$sbatch_file_name"
-sed -i "s|source|source '/software/python-$anaconda_module-el7-x86_64/etc/profile.d/conda.sh'|g" "$sbatch_file_name"
-sed -i "s|conda activate|conda activate $environment_name|g" "$sbatch_file_name"
+# Load the anaconda module
+echo "Loading the Anaconda module"
+module load "python/$anaconda_module"
 
-# Make the executables
-chmod +x "$genx3_file_name" "$sbatch_file_name"
+# Enable conda-forge
+echo "Enable conda-forge"
+conda config --add channels conda-forge
+
+# Create the environment
+conda create -n "$environment_name" python="$python_version" $anaconda_packages --yes
+
+# Activate the environment
+conda activate "$environment_name"
+
+# Update pip
+pip install -U pip setuptools
+
+# Install pypi packages
+pip install $pypi_packages
+
+# Add custom model
+cp $custom_model_path "$HOME/.conda/envs/$environment_name/lib/python$python_version/site-packages/genx/models"
+
+# Elapsed time
+elapsed=$SECONDS
+echo -ne "
+
+Elapsed time: $(($elapsed / 60)) minutes and $(($elapsed % 60)) seconds
+"
+
+# Autostart
+read -rp "Do you want to start GenX3? [Y/n] " selection
+
+if [ -z "$selection" ]; then
+    selection="y"
+fi
+
+case $selection in
+    [yY][eE][sS]|[yY])
+        genx
+        ;;
+    [nN][oO]|[nN])
+        ;;
+    *)
+esac
